1908463d3fb724a7b1f4e7745fb43433866c6427
savdev <savde388@gmail.com>
2025-10-04 08:50:04 -0700

    y

--- app/(component)/login/page.tsx
+++ app/(component)/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import SecurityModal from '../../compo/modalsecurity/page'
+import SecurityModal from '../../compo/modalsecurity'
 
 export default function LoginPage() {
   const [form, setForm] = useState({
